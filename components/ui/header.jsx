83c560fef3733e23aa86e396433a530b8e997aae
0prii0 <gpriyanshu585@gmail.com>
2024-07-19 13:31:32 +0530

    complete nav bar

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,15 +1,29 @@
 import Link from "next/link";
 import {Button} from "./button"
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">header</div>
+        <div className="container mx-auto flex justify-between items-center">
+          {/* logo */}
         <Link href="/">
-        <h1 className="">
-            PG <span>.</span>
+        <h1 className="text-4xl font-semibold">
+            Priyanshu<span className="text-accent">.</span>
         </h1>
         </Link>
+        {/* nav-bar */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+          <button>Hire Me</button>
+          </Link>
+        </div>
+        {/* mobile nav bar */}
+        <div className="xl:hidden">mobile nav
+
+        </div>
+        </div>
     </header>
   )
 }
